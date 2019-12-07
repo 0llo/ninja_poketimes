586e8bcb9049020bc46cc7d7984a379f298f201c
0llo <conan@ecpmail.com>
2019-12-06 17:29:05 -0800

    update

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+      <h4 className="center">About</h4>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere amet
+        earum aut libero laborum? Quaerat magni enim facere expedita. Laborum,
+        sapiente fugit? Nemo at libero harum incidunt saepe, vero nam?
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere amet
+        earum aut libero laborum? Quaerat magni enim facere expedita. Laborum,
+        sapiente fugit? Nemo at libero harum incidunt saepe, vero nam?
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
